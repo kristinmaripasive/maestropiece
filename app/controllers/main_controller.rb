0a89222178c7de94fc7120a732da3bc21078d2fd
Kristin Pasive <kristin.pasive@gmail.com>
2016-08-11 16:40:52 -0400

    fixed routes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,10 @@
 class MainController < ApplicationController
     def index
-binding.pry
+
       if current_user && current_user.artist_box
         redirect_to new_artist_path
       elsif current_user && current_user.music_box
-        binding.pry
+      
         redirect_to new_musician_path
       end
     end
