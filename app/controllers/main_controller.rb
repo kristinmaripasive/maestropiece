3c0028405c5858fb21e95748cf8caa02a253fffa
Kristin Pasive <kristin.pasive@gmail.com>
2016-08-11 13:14:35 -0400

    artist box works

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,9 @@
 class MainController < ApplicationController
-  
+  def index
+  if :artist_box
+    redirect_to new_artist_path
+  else
+    redirect_to new_musician_path
+  end
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,8 @@ Rails.application.routes.draw do
   end
   resources :collabs
 
+
+
+
+
 end
