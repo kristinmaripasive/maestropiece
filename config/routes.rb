69112b8c5c6afd862032b0fd48ca381357385f4f
Kristin Pasive <kristin.pasive@gmail.com>
2016-08-12 11:16:58 -0400

    initial

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root to: "main#show"
     resources :main, only: [:index, :show]
 
-  devise_for :users
+    devise_for :users, controllers: {
+         sessions: 'users/sessions'
+       }
+
   resources :artists do
     resources :arts
   end
